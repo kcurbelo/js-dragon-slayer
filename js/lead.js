fcdcd850ed922d229878cd8537fa9a9a4829bb3c
Kevin Curbelo <kevin.curbelo@gmail.com>
2018-01-02 21:27:52 -0800

    Added some jquery show and animation

--- js/lead.js
+++ js/lead.js
@@ -1,121 +1,67 @@
-// Class 1 weapons ========================================
-// if user selects class 1 then do this
-function classOneSelect(){
-	document.getElementById("class-one-weapons-cont").style.display = "block";
-}
-// Class 1 - Weapon 1
-function c1WeaponOneChoose(){
-	console.log('You have chosen weapon one!');
-	hideClassAndWeaponsCont();
-}
-// Class 1 - Weapon 2
-function c1WeaponTwoChoose(){
-	console.log('You have chosen weapon two!');
-	hideClassAndWeaponsCont();
-}
-// Class 1 - Weapon 3
-function c1WeaponThreeChoose(){
-	console.log('You have chosen weapon three!');
-	hideClassAndWeaponsCont();
-}
-
-// Class 2 weapons ========================================
-// if user selects class 2 then do this
-function classTwoSelect(){
-	document.getElementById("class-two-weapons-cont").style.display = "block";
-}
-// Class 2 - Weapon 1
-function c2WeaponOneChoose(){
-	console.log('You have chosen weapon one!');
-	hideClassAndWeaponsCont();
-}
-// Class 2 - Weapon 2
-function c2WeaponTwoChoose(){
-	console.log('You have chosen weapon two!');
-	hideClassAndWeaponsCont();
-}
-// Class 2 - Weapon 3
-function c2WeaponThreeChoose(){
-	console.log('You have chosen weapon three!');
-	hideClassAndWeaponsCont();
-}
-
-// Class 3 weapons ========================================
-// if user selects class 3 then do this
-function classThreeSelect(){
-	document.getElementById("class-two-weapons-cont").style.display = "block";
-}
-// Class 3 - Weapon 1
-function c3WeaponOneChoose(){
-	console.log('You have chosen weapon one!');
-	hideClassAndWeaponsCont();
-}
-// Class 3 - Weapon 2
-function c3WeaponTwoChoose(){
-	console.log('You have chosen weapon two!');
-	hideClassAndWeaponsCont();
-}
-// Class 3 - Weapon 3
-function c3WeaponThreeChoose(){
-	console.log('You have chosen weapon three!');
-	hideClassAndWeaponsCont();
-}
-
-// Hide class and weapon select
-function hideClassAndWeaponsCont(){
-	document.getElementById("choose-class-cont").style.display = "none";
-	document.getElementById("class-one-weapons-cont").style.display = "none";
-	document.getElementById("class-two-weapons-cont").style.display = "none";
-	document.getElementById("class-three-weapons-cont").style.display = "none";
-}
-
-// Show class images ==========================================
 window.onload = function() {
 
-	$( "#class-1" ).hover(
-	  function() {
-	    $("#class-one-img").stop( true, false ).show( "regular" );
-	    $("#class-two-img").stop( true, false ).hide( "fast" );
-	    $("#class-three-img").stop( true, false ).hide( "fast" );
 
-	  }, function() {
-	    $("#class-one-img").stop( true, false ).fadeOut( "regular" );
-	  }
-	);	
+$( "#class-1" ).hover(
+  function() {
+    $( "#class-one-img" ).addClass("char-select-bor");
+    // console.log('asds');
+  }, 
+  function() {
+    $( "#class-one-img" ).removeClass("char-select-bor");
+    // console.log('asds');
+  }
+);
 
-	$( "#class-2" ).hover(
-	  function() {
-	    $("#class-one-img").stop( true, false ).hide( "fast" );
-	    $("#class-two-img").stop( true, false ).show( "regular" );
-	    $("#class-three-img").stop( true, false ).hide( "fast" );
+$( "#class-2" ).hover(
+  function() {
+    $( "#class-two-img" ).addClass("char-select-bor");
+    // console.log('asds');
+  }, 
+  function() {
+    $( "#class-two-img" ).removeClass("char-select-bor");
+    // console.log('asds');
+  }
+);
 
-	  }, function() {
-	    $("#class-two-img").stop( true, false ).fadeOut( "regular" );
-	    $("#class-one-img").stop( true, false ).show( "regular" );
-	  }
-	);
-
-	$( "#class-3" ).hover(
-	  function() {
-	    $("#class-one-img").stop( true, false ).hide( "fast" );
-	    $("#class-two-img").stop( true, false ).hide( "fast" );
-	    $("#class-three-img").stop( true, false ).show( "regular" );
-
-	  }, function() {
-	    $("#class-three-img").stop( true, false ).fadeOut( "regular" );
-	    $("#class-one-img").stop( true, false ).show( "regular" );
-	  }
-	);		
-
-
-// If second class is chosen 
+$( "#class-3" ).hover(
+  function() {
+    $( "#class-three-img" ).addClass("char-select-bor");
+    // console.log('asds');
+  }, 
+  function() {
+    $( "#class-three-img" ).removeClass("char-select-bor");
+    // console.log('asds');
+  }
+);
 
 
-	$('#class-2').click(function(){
-  		// $(this).data('clicked', true);
-		    $("#class-one-img").hide();
-		    $("#class-two-img").stop( true, false ).show();
-		    $("#class-three-img").hide();		
-	});
+$( "#class-1" ).click(function() {
+	console.log('ss');
+	$( "#class-two-img" ).addClass("hide-it");
+	$( "#class-three-img" ).addClass("hide-it");
+	$( "#class-1-weapons-cont" ).delay(600).show("slow");
+	let classChoice = $( "#class-1" ).html();
+	// console.log(d);
+	$( "#choose-class-title" ).animate({'opacity': 0}, 400, function(){
+        $(this).html("You are now a " + classChoice).animate({'opacity': 1}, 400);    
+    });
+	$( ".classes-cont" ).hide("slow");
+
+})
+
+
+	// Class 1 weapons ========================================
+
+
+	// Hide class and weapon select
+	function hideClassAndWeaponsCont(){
+		document.getElementById("choose-class-cont").style.display = "none";
+		document.getElementById("class-one-weapons-cont").style.display = "none";
+		document.getElementById("class-two-weapons-cont").style.display = "none";
+		document.getElementById("class-three-weapons-cont").style.display = "none";
+	}
+
+	// Show class images ==========================================
+
 }
 

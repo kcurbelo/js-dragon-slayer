b8a405fe22229066e81d6a2dc280cc4c180b9065
Kevin Curbelo <kevin.curbelo@gmail.com>
2018-01-04 15:42:57 -0800

    Added all classes javascript logic

--- js/second.js
+++ js/second.js
@@ -22,18 +22,8 @@ $(document).ready(function() {
   }); 
   
 
-
-  // Class 1 - Weapon 1 ============================================================
-  $( "#c-1-w-1" ).click(function() {
-    console.log('c-1-w-1 was clicked');
-    // Fades out c-1-w cont
-    $( "#c-1-w-cont" ).fadeOut("slow");
-    // Fades out c-2-w cont
-    $( "#c-2-w-cont" ).fadeOut("slow");
-    // Fades out c-3-w cont
-    $( "#c-3-w-cont" ).fadeOut("slow");
-    // Fades in battle countdown
-    $( "#battle-countdown-cont" ).delay("slow").fadeIn("slow");   
+  // Function to start coutdown
+  function startCountdown() {   
     let count=6;
     let counter=setInterval(timer, 1200); //1000 will  run it every 1 second
     function timer(){
@@ -51,10 +41,95 @@ $(document).ready(function() {
       }
       //Shows the number of seconds here
       document.getElementById("countdown-text").innerHTML = count;
-      }     
+      }  
+  }
+  // Function to hide containers after countdown
+  function hideClassCont() {  
+    // Fades out c-1-w cont
+    $( "#c-1-w-cont" ).fadeOut("slow");
+    // Fades out c-2-w cont
+    $( "#c-2-w-cont" ).fadeOut("slow");
+    // Fades out c-3-w cont
+    $( "#c-3-w-cont" ).fadeOut("slow");
+  } 
+  function showBattleCDCont() {
+    $( "#battle-countdown-cont" ).delay("slow").fadeIn("slow");
+  }
+
+
+
+  // Class 1 - Weapon 1 ============================================================
+  $( "#c-1-w-1" ).click(function() {
+    console.log('c-1-w-1 was clicked');
+    // Hides class selection container
+    hideClassCont();
+    // Fades in battle countdown
+    showBattleCDCont(); 
+    // Starts countdown timer
+    startCountdown();
   });
-
-
+  // Class 1 - Weapon 2 ============================================================
+  $( "#c-1-w-2" ).click(function() {
+    console.log('c-1-w-2 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  });  
+  // Class 1 - Weapon 3 ============================================================
+  $( "#c-1-w-3" ).click(function() {
+    console.log('c-1-w-3 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  }); 
+  // ====================
+  // Class 2 
+  // ====================
+  // Class 2 - Weapon 1 ============================================================
+  $( "#c-2-w-1" ).click(function() {
+    console.log('c-2-w-1 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  });     
+  // Class 2 - Weapon 2 ============================================================
+  $( "#c-2-w-2" ).click(function() {
+    console.log('c-2-w-2 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  }); 
+  // Class 2 - Weapon 3 ============================================================
+  $( "#c-2-w-3" ).click(function() {
+    console.log('c-2-w-3 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  });
+  // ====================
+  // Class 3
+  // ====================
+  // Class 3 - Weapon 1 ============================================================
+  $( "#c-3-w-1" ).click(function() {
+    console.log('c-2-w-1 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  });     
+  // Class 3 - Weapon 2 ============================================================
+  $( "#c-3-w-2" ).click(function() {
+    console.log('c-2-w-2 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  }); 
+  // Class 3 - Weapon 3 ============================================================
+  $( "#c-3-w-3" ).click(function() {
+    console.log('c-2-w-3 was clicked');
+    hideClassCont();
+    showBattleCDCont(); 
+    startCountdown();
+  });      
 
   // Misc variables
   let randomNumber = Math.random() >= 0.5; 
@@ -97,9 +172,7 @@ $(document).ready(function() {
     userAttack();
   }); 
 
-  // Calculates dragon dam
 
-  // $( "#u-atk" ).click(function() {
     function userAttack() {    
       // When a dodge is unsuccessful do this:
       if (dodge === false) {
@@ -109,8 +182,8 @@ $(document).ready(function() {
         dodge = randomNumber;
         // Applys damage to health because the silly dragon forgot to dodge
         health = health - charAtk;
-          $("#resp-text").html( charAtkTitle + " attack successful!" );
-          $("#resp-sub-text").html("Dragon health is now " + health );        
+        $("#resp-text").html( charAtkTitle + " attack successful!" );
+        $("#dragon-hp").html( health + "/500");        
         // Console logs health 
         console.log(health)
       }
@@ -134,23 +207,26 @@ $(document).ready(function() {
             dragonAtkTitle = "heavy";
           }
           $("#resp-text").html("Dragon dodged and countered with a " + dragonAtkTitle + " attack" );
-          $("#resp-sub-text").html("Your health is now " + charHealth );
+          $("#char-hp").html( charHealth + "/100");
         // charHealth = charHealth - dragonDamage; 
         console.log('charHealth is now ' + charHealth)     
       }    
       // When health is 0 do this:
       if (health <= 0 ) {
+        health = 0;        
         console.log('Dead');
-        health = 0;
-        $( ".light-atk" ).hide();
+        $("#dragon-hp").html( 0 + "/500");
+        $( "#select-action-cont" ).hide();
+        $( "#done-cont" ).show();
       }
       if (charHealth <= 0 ) {
+        health = 0;        
         console.log('charDead');
-        health = 0;
-        $( ".light-atk" ).hide();
+        $("#char-hp").html( health + "/100");
+        $( "#select-action-cont" ).hide();
+        $( "#done-cont" ).show();
       } 
     }       
-  // });
 
 
 
